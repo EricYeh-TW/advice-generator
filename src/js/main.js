7eb513f8b07390f553ac5e93c6a2f34ecae959ae
Eric Yeh <spu8673@gmail.com>
2022-10-19 01:24:58 +0800

    chore: add html-webpack-plugin, seting build output with asset modules feature: fetch advice from url

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 import '../scss/styles.scss';
-import * as bootstrap from 'bootstrap';
+
+let dice = document.querySelector('#dice');
+
+function fetchAdvice() {
+  const advice__id = document.querySelector('#advice__id');
+  const content = document.querySelector('#advice');
+  const url = 'https://api.adviceslip.com/advice';
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+      let { id, advice } = result.slip;
+
+      advice__id.innerHTML = `ADVICE ${id}`;
+      content.innerHTML = advice;
+
+      console.log(result.slip);
+    });
+}
+
+dice.addEventListener('click', fetchAdvice);
+fetchAdvice();
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development', // 不使用minify
   entry: './src/js/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'images/[hash][ext][query]',
+    assetModuleFilename: 'App/[hash][ext][query]', // 預設Asset Modules輸出位置與檔名
+    clean: true, // 清除上一次build的殘存檔案
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -37,8 +45,12 @@ module.exports = {
         ],
       },
       {
+        // Asset Modules設定特定檔案類型輸出位置與檔名
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
       },
       {
         test: /\.html$/i,
